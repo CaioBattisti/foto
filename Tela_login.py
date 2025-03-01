baa42f3695dce138f4084c9017e8399b7f66b8dc
CaioBattisti <cluizbattisti@gmail.com>
2025-03-01 12:31:52 -0300

    save

--- Tela_login.py
+++ Tela_login.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import massagebox #importa o modulo de caixas de mensagem do tkinter
+from tkinter import messagebox #importa o modulo de caixas de mensagem do tkinter
 from tkinter import ttk#importa o modulo de widgets tematicos do tkinter
 from dataclasses import database#importa a classe Database do modulo DataBase
 
@@ -34,3 +34,42 @@ senhaLabel = Label(RightFrame, text="Senha: ",font=("Century Gothic",20),bg="MID
 senhaLabel.place(x=5, y=150)#posiciona o label no frame direito
 senhaEntry = ttk.Entry(RightFrame, width=30, show=".")#cria um campo de entrada para a senha
 senhaEntry.place(x=120, y=165)#posiciona o campo de entrada
+
+#função de login
+def Login():
+    usuario = usuarioEntry.get()
+    senha = senhaEntry.get()
+
+#conectar ao banco de dado
+    db = database()
+    db.cursor.execute("""SELECT * FROM usuario1 WHERE usuario = %s""",(usuario, senha))
+    VerifiyLogin = db.cursor.fetchone()
+
+#ferificar se o usuario foi encontrado
+    if VerifiyLogin:
+        messagebox.showinfo(title="Info Login", Message ="Acesso Confirmado. Bem Vindo!")
+    else:
+        messagebox.showinfo(title="Info Login", Message="Acesso Negado, Verifique se o cadastro esta no sistema")
+
+#criando botões
+LoginButton = ttk.Button(RightFrame, text="Login",width=15, command=Login)
+LoginButton.place(x=150,y=225)
+
+#função para registrar novo usuario
+def registrar():
+    #removendo botões de Login
+    LoginButton.place(x=5000)
+    RegisterButton.place(x=5000)
+
+#inserindo widgets de cadastro
+    NomeLabel = Label(RightFrame, text="Nome:",font=("Century Gothic", 20),bg="MIDNIGHTBLUE",fg="White")
+    NomeLabel.place(x=5, y=5)
+    NomeEntry = ttk.Entry(RightFrame, width=30)
+    NomeEntry.place(x=120, y=20)
+
+    EmailLabel = Label(RightFrame, text="Email:",font=("Century Gothic",20),bg="MIDNIGHTBLUE",fg="White")
+    EmailLabel.place(x=5,y=40)
+    EmailEntry = ttk.Entry(RightFrame, width=30)
+    EmailEntry.place(x=120,y=55)
+
+    #
