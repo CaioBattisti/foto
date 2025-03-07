cf32f30b7c83de276ab781b07c1f43b4d902a928
CaioBattisti <cluizbattisti@gmail.com>
2025-03-06 21:46:49 -0300

    save

--- Database.py
+++ Database.py
@@ -13,9 +13,9 @@ class Database:
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS 
         usuario1(                    
             idusuario INT AUTO_INCREMENT PRIMARY KEY               
-            nome TEXT (255),              
-            email TEXT (255),               
-            usuario TEXT (255),  
+            nome TEXT (255),                            
+            usuario TEXT (255),
+            email TEXT (255),  
             senha TEXT (255)                           
         );''')
         self.conn.commit()
@@ -24,13 +24,13 @@ class Database:
 
 #Método para registrar um novo usuário no Banco de Dados
     def RegistrarNoBanco(self,nome,email,usuario,senha):
-        self.cursor.execute("INSERT INTO usuario1 (nome,email,usuario,senha) VALUES (%s,%s,%s,%s)",
+        self.cursor.execute("INSERT INTO usuario1 (nome,usuario,email,senha) VALUES (%s,%s,%s,%s)",
                             (nome,email,usuario,senha))
         self.conn.commit()
 
 #Método para alterar os dados de um usuario existente no Banco de Dados
     def alterar(self,idusuario,nome,email,usuario,senha):
-        self.cursor.execute("UPDATE usuario1 SET nome=%s, email=%s, usuario=%s,senha=%s WHERE idusuario=%s",
+        self.cursor.execute("UPDATE usuario1 SET nome=%s, usuario=%s, email=%s, senha=%s WHERE idusuario=%s",
                             (nome,email,usuario,senha,idusuario))
         self.conn.commit()
 
