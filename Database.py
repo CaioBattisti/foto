aaa62fc4d390aa924f8cfe21caaf865766ee24ed
CaioBattisti <cluizbattisti@gmail.com>
2025-03-07 21:11:48 -0300

    salvare

--- Database.py
+++ Database.py
@@ -1,50 +1,46 @@
 import mysql.connector
+MYSQL_HOST ="localhost",
+MYSQL_USER ="root", 
+MYSQL_PASSWORD ="",
+MYSLQ_DATABASE ="caiobattisti_db" 
+def get_connection():
+    return mysql.connector.connect(
+    host=MYSQL_HOST,
+    user=MYSQL_USER,
+    password=MYSQL_PASSWORD,
+    Database=MYSLQ_DATABASE
+)
 
-class Database:
-    def __init__(self):
-        #conecta ao banco de dados
-        return mysql.connector.connect(
-      MYSQL_HOST ="localhost",
-      MYSQL_USER ="root", 
-      MYSQL_PASSWORD ="",
-      MYSLQ_DATABASE ="caiobattisti_db" 
-      )
-        
-    self.cursor = self.conn.cursor()
-    self.cursor.execute('''CREATE TABLE IF NOT EXISTS 
-        usuario1(                    
-            idusuario INT AUTO_INCREMENT PRIMARY KEY               
-            nome TEXT (255),                            
-            usuario TEXT (255),
-            email TEXT (255),  
-            senha TEXT (255)                           
-        );''')
-    self.conn.commit()
-
-    print("Conectando ao Banco de Dados")
-
-#Método para registrar um novo usuário no Banco de Dados
-    def RegistrarNoBanco(self,nome,email,usuario,senha):
-        self.cursor.execute("INSERT INTO usuario1 (nome,usuario,email,senha) VALUES (%s,%s,%s,%s)",
-                            (nome,email,usuario,senha))
-        self.conn.commit()
-
-#Método para alterar os dados de um usuario existente no Banco de Dados
-    def alterar(self,idusuario,nome,email,usuario,senha):
-        self.cursor.execute("UPDATE usuario1 SET nome=%s, usuario=%s, email=%s, senha=%s WHERE idusuario=%s",
-                            (nome,email,usuario,senha,idusuario))
-        self.conn.commit()
-
-#Método para excluir usuario do Banco de Dados
-    def excluir(self,idusuario):
-        self.cursor.execute("DELETE FROM usuario1 WHERE idusuario=%s",(idusuario))
-        self.conn.commit()
-
-#Método para buscar os Dados de um usuario no Banco de Dados
-    def buscar(self,idusuario):
-        self.cursor.execute("SELECT * FROM usuario1 WHERE idusuario=%s",(idusuario))
-        self.conn.commit()
-        
-#Método chamado quando a instancia da classe é destruida
-    def __del__(self):
-        self.conn.close()
+def create_user(nome,telefone,email,usuario,senha):
+    conn=get_connection()
+    cursor=conn.cursor()
+    query="insert usuario(nome,telefone,email,usuario,senha)VALUES(%s,%s,%s,%s,%s)"
+    cursor.execute(query,(nome,telefone,email,usuario,senha))
+    conn.commit()
+    cursor.close()
+    conn.close()
+def read_users():
+    conn=get_connection()
+    cursor=conn.cursor()
+    query="SELECT * FROM usuario"
+    cursor.execute(query)
+    result=cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return result
+def update_user(nome,telefone,email,usuario,senha,user_id):
+    conn=get_connection()
+    cursor=conn.cursor()
+    query="UPDATE usuario SET nome=%s,telefone=%s,email=%s,usuario=%s,senha=%s WHERE idusuario=%s"
+    cursor.execute(query,(nome,telefone,email,usuario,senha,user_id))
+    conn.commit()
+    cursor.close()
+    conn.close()
+def delete_user(user_id):
+    conn=get_connection()
+    cursor=conn.cursor()
+    query="DELETE FROM usuario WHERE idusuario=%s"
+    cursor.execute(query,(user_id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
